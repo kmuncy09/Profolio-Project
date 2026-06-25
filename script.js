35984c49d4850bd6b91ddc40fe2b26284be90bb2
kmuncy09 <kendall.muncy09@gmail.com>
2026-06-25 15:19:51 -0500

    screenshot of projects

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let buttonSubmit= document.querySelector("button");
 let messageInput= document.querySelector("input");
 let thanks=document.querySelector(".thanks");
+let oImages=document.querySelector(".O-images");
 
 buttonSubmit.addEventListener('click', function(){
     messageInput.style.display="none";
