832ba0a53cb4fc4c614866be1a63a0e51829c14e
kmuncy09 <kendall.muncy09@gmail.com>
2026-06-26 09:27:26 -0500

    add carousel

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 let buttonSubmit= document.querySelector("button");
 let messageInput= document.querySelector("input");
 let thanks=document.querySelector(".thanks");
-let oImages=document.querySelector(".O-images");
+let previousButton=document.querySelector(".previous");
+let nextButton=document.querySelector(".next");
+let track=document.querySelector(".carousel-track");
+let slides=document.querySelector(".carousel-slide");
 
 buttonSubmit.addEventListener('click', function(){
     messageInput.style.display="none";
     thanks.style.display="block";
     buttonSubmit.style.display="none";
+});
+let currentIndex = 0;
+let totalSlides=slides.length;
+
+function updateCarousel() {
+    track.style.transform= `translateX(-${currentIndex * 100}%)`;
+}
+nextButton.addEventListener('click', function(){
+    if (currentIndex < totalSlides -1) {
+        currentIndex++;
+    } else {
+        currentIndex=0;
+    }
+    updateCarousel();
+});
+previousButton.addEventListener('click', function(){
+    if (currentIndex > 0) {
+        currentIndex--;
+    } else {
+        currentIndex= totalSlides - 1;
+    }
+    updateCarousel();
 });
