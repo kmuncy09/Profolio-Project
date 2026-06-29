d2428367b6855c07d64baf1dfecbbf6e4c1b89e9
kmuncy09 <kendall.muncy09@gmail.com>
2026-06-29 14:40:39 -0500

    Mobile view, fix carousel, working contact submission

--- script.js
+++ script.js
@@ -1,35 +1,44 @@
-let buttonSubmit= document.querySelector("button");
-let messageInput= document.querySelector("input");
-let thanks=document.querySelector(".thanks");
-let previousButton=document.querySelector(".previous");
-let nextButton=document.querySelector(".next");
-const track=document.querySelector(".carousel-track");
-let slides=document.querySelector(".carousel-slide");
+let buttonSubmit = document.querySelector(".submit");
+let messageInput = document.querySelector("input");
+let thanks = document.querySelector(".thanks");
+let reachOut= document.querySelector(".reach-out");
 
-buttonSubmit.addEventListener('click', function(){
-    messageInput.style.display="none";
-    thanks.style.display="block";
-    buttonSubmit.style.display="none";
-});
+let previousButton = document.querySelector(".previous");
+let nextButton = document.querySelector(".next");
+const track = document.querySelector(".carousel-track");
+let slides = document.querySelectorAll(".carousel-slide");
+
 let currentIndex = 0;
-let totalSlides=slides.length;
+let totalSlides = slides.length;
 
 function updateCarousel() {
-    track.style.transform = `translateX(-${currentIndex * 100}%)`;
+    if (track) {
+        track.style.transform = `translateX(-${currentIndex * 100}%)`};
 }
-nextButton.addEventListener("click", function () {
-    if (currentIndex < totalSlides -1) {
+if (nextButton) {
+    nextButton.addEventListener('click', function(){
+    if (currentIndex < totalSlides - 1) {
         currentIndex++;
     } else {
-        currentIndex=0;
+        currentIndex = 0;
     }
     updateCarousel();
-});
-previousButton.addEventListener("click", function () {
+})};
+if (previousButton) {
+    previousButton.addEventListener('click', function () {
     if (currentIndex > 0) {
         currentIndex--;
     } else {
-        currentIndex= totalSlides - 1;
+        currentIndex = totalSlides - 1;
     }
     updateCarousel();
+})};
+
+buttonSubmit.addEventListener("click", function(){
+    let messageInputValue=messageInput.value;
+    messageInput.style.display="none";
+    thanks.style.display="block";
+    buttonSubmit.style.display="none";
+    messageInputValue = messageInputValue.replace(" ", "%20");
+    window.location.href="mailto:kendall.muncy09@gmail.com? &body="+ messageInputValue;
 });
